fcd7e736143ac1c796c23ab4dad0ec1af114806c
skieffer <kieffer.steve@gmail.com>
2012-11-16 17:58:53 +1100

    Left new DSBPathway files out of last commit.

---
+++ libdsbpe/dsbpathway.h
@@ -0,0 +1,62 @@
+/*
+ * DSBPE plugin - For editing Systems Biology pathways.
+ *
+ * Copyright (C) 2011-2012  Monash University
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ *
+ *
+ * Author(s): Steven Kieffer  <http://skieffer.info>
+*/
+
+#ifndef DSBPATHWAY_H
+#define DSBPATHWAY_H
+
+#include <QList>
+#include <QMap>
+
+#include "dsbreclayout.h"
+
+namespace dunnart {
+
+class DSBBranch;
+class DSBNode;
+
+class DSBPathway : public DSBRecLayout
+{
+public:
+    // Constructors
+    DSBPathway(DSBNode *endpt, QList<DSBBranch*> branches);
+    DSBPathway(DSBBranch *primaryBranch, QList<DSBBranch*> secondaryBranches);
+    // RecLayout methods
+    QSizeF layout();
+    void setRelPt(QPointF p);
+    void drawRelTo(QPointF q);
+    void drawAt(QPointF r);
+    void redraw();
+    // Other
+    QMap<DSBNode*, DSBBranch*> countBranchPoints(QList<DSBBranch*> branches);
+
+private:
+    QPointF m_relpt;
+    QPointF m_basept;
+    QSizeF m_size;
+    DSBNode *m_headNode;
+};
+
+}
+
+#endif // DSBPATHWAY_H
